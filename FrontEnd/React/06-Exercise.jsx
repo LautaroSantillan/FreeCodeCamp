85088620f081924d8b4e92ad58670d5b9dcfccc0
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-08-03 18:28:42 -0300

    I've been doing the course of React -v18

---
+++ FrontEnd/React/06-Exercise.jsx
@@ -0,0 +1,7 @@
+const MyComponent = function() {
+    // Change code below this line
+    return(
+      <div className="customClass">This is my react component</div>
+    );
+    // Change code above this line
+}
