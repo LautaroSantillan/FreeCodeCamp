eb3f86d228f77934b03d76d044f2ad0c89f65146
Lautaro Santillan <santillanlautaro03@gmail.com>
2024-08-14 19:32:38 -0300

    I've been doing the course of React -v29

---
+++ FrontEnd/React/28-Exercise.jsx
@@ -0,0 +1,43 @@
+class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        submit: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value
+      });
+    }
+    handleSubmit(event) {
+      // Change code below this line
+      event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
+      this.setState({
+        submit: this.state.input // Actualiza el estado 'submit' con el valor del estado 'input'
+      });
+      // Change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            {/* Change code below this line */}
+            <input
+              type="text"
+              value={this.state.input}
+              onChange={this.handleChange}
+            />
+            {/* Change code above this line */}
+            <button type='submit'>Submit!</button>
+          </form>
+          {/* Change code below this line */}
+          <h1>{this.state.submit}</h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+}
